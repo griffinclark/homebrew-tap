be9fa8d76155504562e18199542ab91ad2f3d2e6
Griffin <79300656+griffinclark@users.noreply.github.com>
2026-07-22 19:25:11 +0000

    vbear: add 0.1.8 bottle.

--- Formula/vbear.rb
+++ Formula/vbear.rb
@@ -9,6 +9,13 @@ class Vbear < Formula
   sha256 "f0a5a7e416a14d8a8a1b3a04f9651c5764ad8ea61690999184f943f655566881"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/griffinclark/homebrew-tap/releases/download/vbear-0.1.8"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "c85f171df38465ea6b0de31174d774e313abbc8a26846d7f25a7d0e48a6701b5"
+    sha256 cellar: :any_skip_relocation, sequoia:      "14354a61b0bff3e77aedd0de199f09c1f4553e31c3f53dbe2cf05fe929f08b61"
+    sha256 cellar: :any,                 x86_64_linux: "39e6da5078dc9eeb21acdda929c155ac98da596488ab6bc55faa5bc159900a30"
+  end
+
   depends_on "go" => :build
 
   on_linux do
